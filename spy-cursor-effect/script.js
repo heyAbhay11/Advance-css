72d3c47e4c2cf80d9171150d23a2fe89951e160c
Abhay <abhay23012004@gmail.com>
2026-01-02 17:51:02 +0530

    Delete spy-cursor-effect directory

--- spy-cursor-effect/script.js
+++
@@ -1,7 +0,0 @@
-let body = document.querySelector('body')
-
-addEventListener('mousemove',(val)=>{
-    console.log(val.x,val.y)
-    body.style.setProperty('--x',val.clientX +'px')
-    body.style.setProperty('--y',val.clientY +'px')
-})
