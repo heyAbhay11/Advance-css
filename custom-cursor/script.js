eac0704c62ef6ae70854de049e243f8c47f869bc
Abhay <abhay23012004@gmail.com>
2026-01-02 17:48:58 +0530

    Delete custom-cursor directory

--- custom-cursor/script.js
+++
@@ -1,8 +0,0 @@
-let main = document.querySelector('main')
-let cursor = document.querySelector('#cursor')
-
-main.addEventListener('mousemove',function(det){
-cursor.style.left = det.clientX+ 'px'
-cursor.style.top = det.clientY + 'px'
-cursor.style.transform = 'translate(-50%,-50%)'
-})
